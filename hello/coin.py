5b23145c50cf5a318dc07aa2b3ca8ba495e905ea
Vinicius Campanha <vinicampa@gmail.com>
2022-06-03 22:33:32 -0300

    Update coin.py

--- hello/coin.py
+++ hello/coin.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render        
 from django.http import HttpResponse
+from django.http import JsonResponse
 
 from  random import choice
 
 # Create your views here.
 def flip(request):
-    return render(request, choice(['Cara', 'Coroa']))
+    return JsonResponse({'Caiu na moeda: ':choice(['Cara', 'Coroa'])})
 
